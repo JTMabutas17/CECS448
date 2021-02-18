b0287308b790702fee663b6f4966ef66312889ae
Mike Zeng <38231969+miek0@users.noreply.github.com>
2021-02-18 01:17:27 -0800

    this is a test js

---
+++ project1/basic.js
@@ -0,0 +1,11 @@
+function fetchCSV() {
+    var participant = document.getElementById("participant").value;
+    if(participant == "c1")
+    {
+        document.getElementById("table").style.color = "white"
+    } else if (participant == "c2") {
+        document.getElementById("table").style.color = "Yellow"
+    } else if (participant == "c3") {
+    document.getElementById("table").style.color = "Red"
+    }
+}
