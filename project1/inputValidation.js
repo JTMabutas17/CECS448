9eb0d601173a3c4e3bd25e008a7a4836e6878c11
Justin Mabutas <justin.troy.mabutas@gmail.com>
2021-02-18 13:39:26 -0800

    Programmed submit button

--- project1/inputValidation.js
+++ project1/inputValidation.js
@@ -1,5 +1,4 @@
 function checkForm() {
-     
     var success = true;
 
     var username = document.getElementById('username');
@@ -9,11 +8,23 @@ function checkForm() {
     var confirmPassword = document.getElementById('confirmPassword');
     var dateOfBirth = document.getElementById('dateOfBirth');
 
-    var allElements = [username, firstName, lastName, password, confirmPassword, dateOfBirth];
-    for(element in allElements) {
-        if(element.value.length == 0) {
-            element.value = "Input required";
+    var usernameLabel = document.getElementById('usernameLabel');
+    var firstNameLabel = document.getElementById('firstNameLabel');
+    var lastNameLabel = document.getElementById('lastNameLabel');
+    var passwordLabel = document.getElementById('passwordLabel');
+    var confirmPasswordLabel = document.getElementById('confirmPasswordLabel');
+    var dateOfBirthLabel = document.getElementById('dateOfBirthLabel');
+
+
+    var allInputs = [username, firstName, lastName, password, confirmPassword, dateOfBirth];
+    var allLabels = [usernameLabel, firstNameLabel, lastNameLabel, passwordLabel, confirmPasswordLabel, dateOfBirthLabel];
+    for(var i = 0; i < allInputs.length; i++) {
+        allLabels[i].style.color = "black";
+        if(allInputs[i].value.length == 0) {
+            allLabels[i].style.color = "red";
             success = false;
         }
     }
+    if(success) alert("You have successfully submitted this form.");
+    else alert("Enter required fields.");
 };
