2531008fb66da6fc02208188e88c49c5817510ed
Mike Zeng <38231969+miek0@users.noreply.github.com>
2021-02-18 18:48:36 -0800

    trying to do the upload image

---
+++ project1/backend.js
@@ -0,0 +1,100 @@
+var loadFile = function(event) {
+    document.getElementById('progressBar').style.display = 'initial';
+    start(0);
+    
+    var x = setTimeout(function() {
+        
+        document.getElementById('i').style.display = 'none';
+        document.getElementById('output').style.display = 'initial';
+        var image = document.getElementById('output');
+        image.src = URL.createObjectURL(event.target.files[0]);
+    }, 1100);
+};
+
+function start(al) {
+     
+    var bar = document.getElementById('progressBar');
+    var status = document.getElementById('status');
+    status.innerHTML = al + "%<br>";
+    bar.value = al;
+    al++;
+    
+    var sim = setTimeout(function() {
+    
+        start(al);
+    }, 10);
+    
+    if (al == 100) {
+      status.innerHTML = "100%<br>";
+      status.innerHTML = "Image Uploaded<br>";
+      bar.value = 100;
+      clearTimeout(sim);
+    }
+};
+
+function reloadpage(){
+    alert("Account registered!")
+    location.reload();
+    return false;
+}
+
+function checkForm() {
+    var success = true;
+
+    var username = document.getElementById('username');
+    var firstName = document.getElementById('firstName');
+    var lastName = document.getElementById('lastName');
+    var password = document.getElementById('password');
+    var confirmPassword = document.getElementById('confirmPassword');
+    var dateOfBirth = document.getElementById('dateOfBirth');
+
+    var usernameLabel = document.getElementById('usernameLabel');
+    var firstNameLabel = document.getElementById('firstNameLabel');
+    var lastNameLabel = document.getElementById('lastNameLabel');
+    var passwordLabel = document.getElementById('passwordLabel');
+    var confirmPasswordLabel = document.getElementById('confirmPasswordLabel');
+    var dateOfBirthLabel = document.getElementById('dateOfBirthLabel');
+
+
+    var allInputs = [username, firstName, lastName, password, confirmPassword, dateOfBirth];
+    var allLabels = [usernameLabel, firstNameLabel, lastNameLabel, passwordLabel, confirmPasswordLabel, dateOfBirthLabel];
+    for(var i = 0; i < allInputs.length; i++) {
+        allLabels[i].style.color = "black";
+        if(allInputs[i].value.length == 0) {
+            allLabels[i].style.color = "red";
+            success = false;
+        }
+    }
+    if(success) alert("You have successfully submitted this form.");
+    else alert("Enter required fields.");
+};
+
+Image.prototype.load = function(url){
+    var thisImg = this;
+    var xmlHTTP = new XMLHttpRequest();
+    xmlHTTP.open('GET', url,true);
+    xmlHTTP.responseType = 'arraybuffer';
+    xmlHTTP.onload = function(e) {
+        var blob = new Blob([this.response]);
+        thisImg.src = window.URL.createObjectURL(blob);
+    };
+    xmlHTTP.onprogress = function(e) {
+        thisImg.completedPercentage = parseInt((e.loaded / e.total) * 100);
+    };
+    xmlHTTP.onloadstart = function() {
+        thisImg.completedPercentage = 0;
+    };
+    xmlHTTP.send();
+};
+Image.prototype.completedPercentage = 0;
+
+var img = document.getElementById('file');
+img.load(target.files[0]);
+document.getElementById("progress").appendChild(img);
+
+// var _img = document.getElementById('file');
+// var newImg = new Image;
+// newImg.onload = function() {
+//     _img.src = this.src;
+// }
+// newImg.src = URL.createObjectURL(target.files[0]);
