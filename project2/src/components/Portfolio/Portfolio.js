dca4bdcfe59617af3199036f4f1b0bc15c18120a
JTMabutas17 <justin.troy.mabutas@gmail.com>
2021-03-11 18:48:31 -0800

    Pre file structure change

--- project2/src/components/Portfolio/Portfolio.js
+++ project2/src/components/Portfolio/Portfolio.js
@@ -1,35 +1,45 @@
 import React, { Component } from 'react';
+
 export default class Porfolio extends Component {
   render() {
     let data = this.props.portfolioData;
     return (
       <section id="hokage">
-        <div className="hokage-content">
-          <ul>
-            <li>
-              <img className="hokage-pic" src="images/Hokage_Hashirama.png" alt="" />
-            </li>
+        <div className="hokage-container">
+          {/* <div className="hokage-content"> */}
+            <ul>
+              <li>
+                <div className="hokage-pic hashirama" >
+                  <p>First hokage</p>
+                </div>
+              </li>
 
-            <li>
-              <img className="hokage-pic" src="images/Tobirama_Senju.png" alt="" />
-            </li>
+              <li>
+                <img className="hokage-pic" src="images/Tobirama_Senju.png" alt="" />
+                <p>Second hokage</p>
+              </li>
 
-            <li>
-              <img className="hokage-pic" src="images/The_Third_Hokage.png" alt="" />
-            </li>
+              <li>
+                <img className="hokage-pic" src="images/The_Third_Hokage.png" alt="" />
+                <p>Third hokage</p>
+              </li>
 
-            <li>
-              <img className="hokage-pic" src="images/Hokage_Minato.png" alt="" />
-            </li>
+              <li>
+                <img className="hokage-pic" src="images/Hokage_Minato.png" alt="" />
+                <p>Fourth hokage</p>
+              </li>
 
-            <li>
-              <img className="hokage-pic" src="images/Tsunade_as_Hokage.png" alt="" />
-            </li>
+              <li>
+                <img className="hokage-pic" src="images/Tsunade_as_Hokage.png" alt="" />
+                <p>Fifth hokage</p>
+              </li>
 
-            <li>
-              <img className="hokage-pic" src="images/Sixth_Hokage.png" alt="" />
-            </li>
-          </ul>
+              <li>
+                <img className="hokage-pic" src="images/Sixth_Hokage.png" alt="" />
+                <p>Sixth hokage</p>
+              </li>
+            </ul>
+          {/* </div> */}
         </div>
       </section>
     );
