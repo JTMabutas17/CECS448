0132cdf5e64bf3bf7603e4a6d5fd727fd6088c51
JTMabutas17 <justin.troy.mabutas@gmail.com>
2021-03-11 17:48:22 -0800

    Joey's newest version of the project.

--- project2/src/components/Portfolio/Portfolio.js
+++ project2/src/components/Portfolio/Portfolio.js
@@ -4,37 +4,33 @@ export default class Porfolio extends Component {
     let data = this.props.portfolioData;
     return (
       <section id="hokage">
-        <ul>
-           <li>
-             <a href="">Behance</a>
-             <img className="hokage-pic"  src="images/Hokage_Hashirama.png" alt="" />
-           </li>
-           
+        <div className="hokage-content">
+          <ul>
             <li>
-             <a href="">Flickr</a>
-             <img className="hokage-pic"  src="images/Tobirama_Senju.png" alt="" />
-           </li>
-           
+              <img className="hokage-pic" src="images/Hokage_Hashirama.png" alt="" />
+            </li>
+
             <li>
-             <a href="">LinkedIn</a>
-             <img className="hokage-pic"  src="images/The_Third_Hokage.png" alt="" />
-           </li>
-           
-           <li>
-             <a href="">Faceboook</a>
-             <img className="hokage-pic"  src="images/Hokage_Minato.png" alt="" />
-           </li>
-           
-           <li>
-             <a href="">Twitter</a>
-             <img className="hokage-pic"  src="images/Tsunade_as_Hokage.png" alt="" />
-           </li>
-           
-           <li>
-             <a href="">Twitter</a>
-             <img className="hokage-pic"  src="images/Sixth_Hokage.png" alt="" />
-           </li>
-        </ul>
+              <img className="hokage-pic" src="images/Tobirama_Senju.png" alt="" />
+            </li>
+
+            <li>
+              <img className="hokage-pic" src="images/The_Third_Hokage.png" alt="" />
+            </li>
+
+            <li>
+              <img className="hokage-pic" src="images/Hokage_Minato.png" alt="" />
+            </li>
+
+            <li>
+              <img className="hokage-pic" src="images/Tsunade_as_Hokage.png" alt="" />
+            </li>
+
+            <li>
+              <img className="hokage-pic" src="images/Sixth_Hokage.png" alt="" />
+            </li>
+          </ul>
+        </div>
       </section>
     );
   }
