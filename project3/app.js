0ddf91cc279a8c838c90138de5c65bfb71cf644b
JTMabutas17 <thisjuicetin@gmail.com>
2021-03-25 14:14:58 -0700

    Added changeable questions/answers to app.js

--- project3/app.js
+++ project3/app.js
@@ -14,15 +14,21 @@ angular.module("angular", [])
 					{ Name: "Passive/Active", Value: 50 }
 				], 
 				Bio: "Includes personal hobbies and interests.",
-				Drink_pref: [
-					"Unsweet",
-					"Lightly Sweet",
-					"Cold/iced"
-				], 
-				Food_pref: [
-					"Cajun Fries",
-					"Popcorn Chicken"
-				], 
+
+				DrinkQuestion1: "Do you like your drinks served hot or cold?",
+				DrinkAnswer1: "Answer 1",
+				DrinkQuestion2: "How sweet do you like your drinks?",
+				DrinkAnswer2: "Answer 2",
+				DrinkQuestion3: "What size do you normally order?",
+				DrinkAnswer3: "Answer 3",
+
+				FoodQuestion1: "What is your favorite food to order?",
+				FoodAnswer1: "Answer 1",
+				FoodQuestion2: "How often do you order food when you get boba?",
+				FoodAnswer2: "Answer 2",
+				FoodQuestion3: "How important are food options when visiting a boba place?",
+				FoodAnswer3: "Answer 3",
+
 				Ideal_atmos: [
 					{ Name: "Quiet", Value: 75 },
 					{ Name: "Music", Value: 80 },
