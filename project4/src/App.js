501c1ab8e2cd49b49a4e6b6cb1d066c37c3d86ea
Mike Zeng <38231969+miek0@users.noreply.github.com>
2021-04-23 02:26:39 -0700

    footer + prev button

--- project4/src/App.js
+++ project4/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
 
 import Navbar from './components/Navbar';
 import Header from './components/Home';
@@ -35,6 +36,7 @@ class App extends Component {
             <Route path="/CreateAccount" component={CreateAccount} />
           </Switch>
         </div>
+        <Footer />
       </Router>
     );
   }
