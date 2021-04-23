f3ec6aa3fe04edf34326446c3215f9ccd3596902
Joey Tran <joeytran1999@gmail.com>
2021-04-22 20:26:04 -0700

    Finalized Sign In/Create Account

--- project4/src/components/CreateAccount.js
+++ project4/src/components/CreateAccount.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-export default class SignIn extends Component {
+export default class CreateAccount extends Component {
   render() {
     return (
       <section id="create">
-        <div className = "registrationForm">
+        <div className = "create-container">
+        <div className = "createForm-container">
+        <div className = "createForm">
             <form>
-                <h1>Registration Form</h1>
+                <h1>Create an Account</h1>
     
                 <div class = "textBox">
-                    <label for="username" id="usernameLabel">Username:</label><br/>
-                    <input type="text" id="username" name="username" /> <br/>
-                    <label for="firstName" id="firstNameLabel">First Name:</label><br/>
-                    <input type="text" id="firstName" name="firstName" /> <br/>
-                    <label for="lastName" id="lastNameLabel">Last Name:</label><br/>
-                    <input type="text" id="lastName" name="lastName" /> <br/>
-                    <label for="password" id="passwordLabel">Password:</label> <br/>
-                    <input type="password" id="password" name="password" /> <br/>
-                    <label for="confirmPassword" id="confirmPasswordLabel">Confirm Password:</label> <br/>
-                    <input type="password" id="confirmPassword" name="confirmPassword" /> <br/>
-                    <label for="dateOfBirth" id="dateOfBirthLabel">Date of Birth:</label> <br/>
-                    <input type="date" id="dateOfBirth" name="dateOfBirth" /> <br/>
+                    <label for="username" id="usernameLabel">Username:</label>
+                    <input type="text" id="username" name="username" /> 
+                    <label for="firstName" id="firstNameLabel">First Name:</label>
+                    <input type="text" id="firstName" name="firstName" /> 
+                    <label for="lastName" id="lastNameLabel">Last Name:</label>
+                    <input type="text" id="lastName" name="lastName" /> 
+                    <label for="password" id="passwordLabel">Password:</label> 
+                    <input type="text" id="password" name="password" /> 
+                    <label for="confirmPassword" id="confirmPasswordLabel">Confirm Password:</label> 
+                    <input type="text" id="confirmPassword" name="confirmPassword" /> 
+                    <label for="dateOfBirth" id="dateOfBirthLabel">Date of Birth:   </label> 
+                    <input type="date" id="dateOfBirth" name="dateOfBirth" /> 
                 </div>
-                
+                <br/>
                 <Link to="/">
                   <button type="button" class = "button" onclick="checkForm()">Sign In</button>
                 </Link>
             </form>
+        </div>
+        </div>
         </div>
       </section>
     );
--- project4/src/components/SignIn.js
+++ project4/src/components/SignIn.js
@@ -4,20 +4,23 @@ export default class SignIn extends Component {
   render() {
     return (
       <section id="signin">
+        <div className = "signIn-container">
+        <div className = "registrationForm-container">
         <div className = "registrationForm">
             <form>
-                <h1>Registration Form</h1>
-    
+                <h1>Sign In</h1>
                 <div class = "textBox">
                     <label for="username" id="usernameLabel">Username:</label><br/>
                     <input type="text" id="username" name="username" /> <br/>
                     <label for="password" id="passwordLabel">Password:</label> <br/>
-                    <input type="password" id="password" name="password" /> <br/>
+                    <input type="text" id="password" name="password" /> <br/>
                 <Link to="/">
                   <button type="button" class = "button" onclick="checkForm()">Sign In</button>
                 </Link>
                 </div>
             </form>
+        </div>
+        </div>
         </div>
       </section>
     );
